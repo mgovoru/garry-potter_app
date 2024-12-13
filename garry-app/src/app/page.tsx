d511a3b7f51baed1a99f7892d6a9ec68d41f19f8
Maria Govorukhina <m.govoru@yandex.ru>
2024-12-13 18:57:11 +0500

    feat: add select on page with cards

--- garry-app/src/app/page.tsx
+++ garry-app/src/app/page.tsx
@@ -1,19 +1,46 @@
-'use client'
-import { useEffect } from "react";
-import CardHero from "./components/Card/page";
-import styles from "./page.module.css";
-import axios from "axios";
-import { store } from "./store";
-import { addheroes } from "./heroesSlice";
-import { Character } from "./types";
-import React from "react";
+'use client';
+import React from 'react';
+import { useEffect } from 'react';
+import CardHero from './components/Card/page';
+import styles from './page.module.css';
+import axios from 'axios';
+import { store } from './store';
+import { addheroes } from './heroesSlice';
+import { Character } from './types';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import { styled } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+
+const BootstrapInput = styled(InputBase)(({}) => ({
+  '& .MuiInputBase-input': {
+    padding: '10px 26px 10px 12px',
+    border: '1px solid #ced4da',
+    color: 'white',
+    fontFamily: 'var(--font-fontdiner-sans)',
+    '&:focus': {
+      borderColor: 'yellow',
+      color: 'red',
+    },
+  },
+}));
 
 export default function Home() {
   const [loading, setLoading] = React.useState(true);
+  const [favorite, setFavorite] = React.useState('all');
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setFavorite(event.target.value as string);
+  };
+
   useEffect(() => {
     async function getHeroes() {
       try {
-        const response = await axios.get('https://hp-api.onrender.com/api/characters');
+        const response = await axios.get(
+          'https://hp-api.onrender.com/api/characters'
+        );
         store.dispatch(addheroes(response.data.slice(0, 20) as Character[]));
         setLoading(false);
       } catch (error) {
@@ -26,8 +53,48 @@ export default function Home() {
     <div className={styles.page}>
       <main className={styles.main}>
         <div className={styles.container}>
-          {!loading && store.getState().map((hero) => { return <CardHero hero={hero} key={hero.id} /> })}
-          </div>
+          <FormControl fullWidth>
+            <InputLabel
+              id='demo-simple-select-label'
+              sx={{
+                color: 'white',
+                fontFamily: 'var(--font-fontdiner-sans)',
+                '&.Mui-focused': { color: 'red' },
+              }}
+            >
+              Choose heroes
+            </InputLabel>
+            <Select
+              labelId='demo-simple-select-label'
+              id='demo-simple-select'
+              value={favorite}
+              label='Age'
+              onChange={handleChange}
+              input={<BootstrapInput />}
+            >
+              <MenuItem
+                value={'all'}
+                sx={{
+                  fontFamily: 'var(--font-fontdiner-sans)',
+                }}
+              >
+                All
+              </MenuItem>
+              <MenuItem
+                value={'favorite'}
+                sx={{
+                  fontFamily: 'var(--font-fontdiner-sans)',
+                }}
+              >
+                Favorite
+              </MenuItem>
+            </Select>
+          </FormControl>
+          {!loading &&
+            store.getState().map((hero) => {
+              return <CardHero hero={hero} key={hero.id} />;
+            })}
+        </div>
       </main>
     </div>
   );
